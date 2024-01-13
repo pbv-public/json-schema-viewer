efe68a5ea1b5f157494d5918a6b7c73605b577a7
pbv <dev@pb.vision>
2024-01-13 12:23:00 -0800

    customize how key schemas are chosen

--- scripts/collect_schemas.py
+++ scripts/collect_schemas.py
@@ -3,12 +3,12 @@ import os
 import sys
 
 
-def main(path_to_schema_json_files):
+def main(path_to_schema_json_files, prefixes_to_ignore):
     schemas = {}
-    for x in os.listdir(path_to_schema_json_files):
-        if not x.endswith(".schema.json"):
+    for schema_id in os.listdir(path_to_schema_json_files):
+        if not schema_id.endswith(".schema.json"):
             continue
-        fn = os.path.join(path_to_schema_json_files, x)
+        fn = os.path.join(path_to_schema_json_files, schema_id)
         schema = json.loads(open(fn, 'r').read())
         schema_id = schema.get('$id')
         if schema_id:
@@ -16,9 +16,18 @@ def main(path_to_schema_json_files):
     assert schemas, 'expected to find at least one schema file with a $id'
     script_dir = os.path.dirname(__file__)
     output_fn = os.path.join(script_dir, '../src/generated-schemas.js')
+    key_schema_ids = []
+    for schema_id in schemas:
+        ok = True
+        for prefix_to_ignore in prefixes_to_ignore:
+            if schema_id.startswith(prefix_to_ignore):
+                ok = False
+                break
+        if ok:
+            key_schema_ids.append(schema_id)
     output_data = dict(
         schemas=schemas,
-        key_schema_ids=[x for x in schemas.keys() if '/types' not in x]
+        key_schema_ids=key_schema_ids
     )
     data_str = json.dumps(output_data, indent=2, sort_keys=True)
     open(output_fn, 'w').write(f'export const schemas = {data_str}')
@@ -26,4 +35,4 @@ def main(path_to_schema_json_files):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2].split(',') if len(sys.argv) > 2 else [])
