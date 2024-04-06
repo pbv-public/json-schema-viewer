5455783bf9215019eca1e3ec209b357049a261e2
pbv <dev@pb.vision>
2024-04-06 12:00:00 -0700

    add global shim to workaround typed-array-byte-offset bug

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    // by default, Vite doesn't include shims for NodeJS which v1.0.2 of the
+    // typed-array-byte-offset which is an indirect dependency requires
+    global: {}
+  }
 })
